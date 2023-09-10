029f50acb65d853b9882228cf7b9c88e1ea788c9
Luis Vasquez <vluis5457@gmail.com>
2023-09-09 20:42:08 -0600

    Restableciendo front

--- cms_front/src/main.jsx
+++ cms_front/src/main.jsx
@@ -1,37 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Login from "./routes/Login.jsx";
-import SignUp from "./routes/SignUp";
-import Dashboard from "./routes/Dashboard";
-import ProtectedRoute from "./routes/ProtectedRoute.jsx";
-import AuthProvider from "./auth/AuthProvider.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/registrar",
-    element: <SignUp />,
-  },
-  {
-    path: "/",
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
