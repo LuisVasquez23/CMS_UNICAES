12e14966c1f4508c9fbb977b8631b54d3f1c54a1
Luis Vasquez <vluis5457@gmail.com>
2023-09-13 10:45:17 -0600

    Estable para avance 1

--- cms_front/src/components/home/Home.jsx
+++ cms_front/src/components/home/Home.jsx
@@ -1,10 +1,8 @@
-import React from "react";
 import Cookies from "js-cookie";
-import 'font-awesome/css/font-awesome.min.css';
-import { Link } from 'react-router-dom';
+import "font-awesome/css/font-awesome.min.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-
   const userName = Cookies.get("userName");
   const pageCount = Cookies.get("pageCount");
 
@@ -13,39 +11,38 @@ const Home = () => {
   return (
     <>
       <h2 className="text-center mt-5 fw-semibold">Bienvenido, {userName}</h2>
-      <p class="text-center fw-light">Te presentamos un resumen de tu actividad en nuestro sitio web</p>
+      <p className="text-center fw-light">
+        Te presentamos un resumen de tu actividad en nuestro sitio web
+      </p>
 
       <div className="container mt-5">
         <div className="row text-center">
+          <div className="col-md-12">
+            <div className="card text-dark  mb-3">
+              <div className="card-header fw-bold fs-5 text-primary-emphasis">
+                <i className="fw-bold fa fa-file"></i> Paginas creadas
+              </div>
+              <div className="card-body text-center">
+                {pageCountNumber > 0 ? (
+                  <h5 className="card-title display-5">{pageCount}</h5>
+                ) : (
+                  <p className="fw-semibold mt-3 mb-3">
+                    No tienes páginas creadas.
+                  </p>
+                )}
 
-            <div className="col-md-12">
-                <div className="card text-dark  mb-3">
-                    <div className="card-header fw-bold fs-5 text-primary-emphasis">
-                    
-                    <i className="fw-bold fa fa-file"></i>  Paginas creadas
-
-                    </div>
-                    <div className="card-body text-center">
-                    {pageCountNumber > 0 ? (
-                        <h5 className="card-title display-5">
-                            {pageCount}
-                        </h5>
-                    ) : ( 
-                      <p className="fw-semibold mt-3 mb-3">No tienes páginas creadas.</p>
-                    )}
-
-                        <Link to="/dashboard/paginas/create" className="btn btn-success mt-2">
-                          <i className="fa fa-plus me-1"></i>
-                          Crear una nueva pagina
-                        </Link>
-
-                    </div>
-                </div>
+                <Link
+                  to="/dashboard/paginas/create"
+                  className="btn btn-success mt-2"
+                >
+                  <i className="fa fa-plus me-1"></i>
+                  Crear una nueva pagina
+                </Link>
+              </div>
             </div>
-
+          </div>
         </div>
-    </div>
-    
+      </div>
     </>
   );
 };
--- cms_front/src/components/paginas/Paginascreate.jsx
+++ cms_front/src/components/paginas/Paginascreate.jsx
@@ -1,222 +1,197 @@
-import Cookies from "js-cookie";
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./paginas.css";
 
 const Paginascreate = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
 
-const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-const toggleMenu = () => {
+  const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-};
+  };
 
   const [data, setData] = useState([]);
-  const [selectedHtml, setSelectedHtml] = useState('');
+  const [etiquetaArrastrada, setEtiquetaArrastrada] = useState(null);
   const [isDragging, setIsDragging] = useState(false);
-  const [previewHtml, setPreviewHtml] = useState('');
+  const [previewHtml, setPreviewHtml] = useState("");
+  const [contenidoHtml, setContenidoHtml] = useState("");
 
-useEffect(() => {
-      // Realizamos una solicitud a la API
-      fetch('http://127.0.0.1:8000/api/etiquetas')
+  useEffect(() => {
+    // Realizamos una solicitud a la API
+    fetch("http://127.0.0.1:8000/api/etiquetas")
       .then((response) => response.json())
       .then((data) => setData(data))
-      .catch((error) => console.error('Error fetching data:', error));
-}, []);                       
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
-useEffect(() => {
+  useEffect(() => {
     // Actualiza la vista previa cuando se cambia el contenido principal
-    setPreviewHtml(selectedHtml);
-}, [selectedHtml]);
+    const bootstrapHtml = `<!doctype html>
+                        <html lang="en">
 
-const handleDragStart = (e, estructuraHtml) => {
-    e.dataTransfer.setData('text/plain', estructuraHtml);
+                        <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <title>Bootstrap demo</title>
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+                        </head>
+
+                        <body>
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+                        ${contenidoHtml} <!-- Contenido HTML -->
+                        </body>
+                        </html>`;
+    setPreviewHtml(bootstrapHtml);
+  }, [contenidoHtml]);
+
+  const handleDragStart = (e, estructuraHtml) => {
+    e.dataTransfer.setData("text/plain", estructuraHtml);
+    setEtiquetaArrastrada(estructuraHtml);
     setIsDragging(true);
-};
+  };
 
-const handleDragEnd = () => {
-setIsDragging(false);
-};
+  const handleDragEnd = () => {
+    setIsDragging(false);
+  };
 
-const handleTextAreaChange = (e) => {
-setSelectedHtml(e.target.value);
-};
+  const handleTextAreaChange = (e) => {
+    setContenidoHtml(e.target.value);
+  };
 
+  const handleDrop = (e) => {
+    e.preventDefault();
+    if (etiquetaArrastrada) {
+      setContenidoHtml(contenidoHtml + etiquetaArrastrada);
+      setEtiquetaArrastrada(null);
+    }
+  };
 
-
-const handleDelete = () => {
-setSelectedHtml('');
-};
-
-const getInnerHtml = (html) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
-  return doc.body.textContent;
-};
-
-
-useEffect(() => {
-const bootstrapHtml = `<!doctype html>
-<html lang="en">
-
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Bootstrap demo</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
- </head>
-
-<body>
-
-<h1>Hello, world!</h1>
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</body>
-</html>`;
-          
-setSelectedHtml(bootstrapHtml);
-}, []);
-
-
-
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <div className="flex">
-      <div
-        style={{
-          margin: '0',
-          padding: '0',
-          display: 'flex',
-        }}
-      >
-        <div className="container-fluid mt-2">
-          <div className="row">
-            <div className="col-md-2">
-              <div
-                className="sidebar mt-5"
-                style={{
-                  width: '14rem',
-                  height: 'calc(100vh - 52px)',
-                  backgroundColor: 'rgb(248,249,250)',
-                  border: '1px solid #ccc',
-                }}
-              >
-                <ul className="nav flex-column">
-                  <li className="nav-item">
-                    <div className="nav-button">
-                      <button
-                        className="nav-link text-dark text-center fs-5 fw-semibold mt-5 ms-3"
-                        onClick={toggleMenu}
-                      >
-                        Etiquetas HTML
-                      </button>
-                      {isMenuOpen && (
-                        <ul className="sub-menu text-success ms-3 ">
+    <div className="container-fluid mt-4">
+      <div className="row">
+        <div className="col-md-2">
+          <div className="sidebar">
+            <ul className="nav flex-column">
+              <li className="nav-item" id="nav-item">
+                <div className="nav-button">
+                  <button className="tag-header" onClick={toggleMenu}>
+                    Etiquetas HTML <i className="fa fa-code fw-bold"></i>
+                  </button>
+                  {isMenuOpen && (
+                    <ul className="sub-menu text-success ">
                       {data.length > 0 ? (
-                            data.map((item) => (
-                              <li
-                                key={item.id}
-                                draggable
-                                onDragStart={(e) => handleDragStart(e, item.estructura_html)}
-                                onDragEnd={handleDragEnd}
-                                className="sub-menu-item text-dark"
-                              >
-                                {item.nombre}
-                              </li>
-                            ))
-                          ) : (
-                            <li className="sub-menu-item text-dark">No se encontraron etiquetas</li>
-                          )}
-                        </ul>
+                        data.map((item) => (
+                          <li
+                            key={item.id}
+                            draggable
+                            onDragStart={(e) =>
+                              handleDragStart(e, item.estructura_html)
+                            }
+                            onDragEnd={handleDragEnd}
+                            className="text-dark"
+                          >
+                            {"<" + item.nombre + "/>"}
+                          </li>
+                        ))
+                      ) : (
+                        <li className="text-dark">
+                          No se encontraron etiquetas
+                        </li>
                       )}
-                    </div>
-                  </li>
-                </ul>
-              </div>
+                    </ul>
+                  )}
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="col-md-10 mt-1">
+          <div className="card ms-2 mb-2">
+            <div className="card-header fw-bold">
+              <i className="fa fa-file"></i>
+              Creación de Página
             </div>
-  
-            {/* Contenido principal */}
-            <div className="col-md-10 mt-1">
-
-              <div className="card ms-2 mb-2">
-              <div class="card-header fw-bold">
-              <i className="fa fa-file me-1 ms-1"></i>
-
-                Creación de Pagina
-                            
-              </div>
-                <div className="card-body">
-
-                  <form>
-
-                  <div className="row">
-                    {/* Primera columna */}
-                    <div className="col-md-6">
-                      <div className="mb-3">
-                        <label className="mb-1" htmlFor="title">Título:</label>
-                        <input type="text" className="form-control" name="title" required/>
-                      </div>
-
-                      <div className="mb-3 mt-4">
-                        <label className="mb-1" htmlFor="HTMLContent">Contenido HTML:</label>
-                        <textarea
-                                placeholder="Arrastra y suelta etiquetas aquí para posicionarlas o editarlas"
-                                value={selectedHtml}
-                                onChange={handleTextAreaChange}
-                                rows={16}
-                                cols={100}
-                                style={{ resize: 'none', spellCheck: 'false' }}
-                                className="form-control"
-                                name="HTMLContent"
-                                required
-                              ></textarea>
-                      </div>
-
-                    </div>
-
-                    
-                    <div className="col-md-6">
-
-                      <div className="mb-3">
-                        <label className="mb-1" htmlFor="url">URL:</label>
-                        <input type="text" className="form-control" name="url" required/>
-                      </div>
-
-                      <div className="mb-3 mt-4">
-                        <label className="mb-1">Resultado:</label>
-                        <iframe
-                                title="Vista previa del sitio web"
-                                srcDoc={previewHtml}
-                                style={{ width: '100%', height: '400px', border: '1px solid #ccc', borderRadius: '5px' }}
-                              ></iframe>
-                      </div>
-
-                      
+            <div className="card-body">
+              <form>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="mb-3">
+                      <label className="mb-1" htmlFor="title">
+                        Título:
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="title"
+                        required
+                      />
                     </div>
                   </div>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-1">
-                  <button class="btn btn-primary me-md-2" type="submit">Crear pagina</button>
-                  <button class="btn btn-danger" type="button">Borrar contenido html</button>
-                </div>
-         
-                  </form>
 
+                  <div className="col-md-6">
+                    <div className="mb-3">
+                      <label className="mb-1" htmlFor="url">
+                        URL:
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="url"
+                        required
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div class="card-footer text-body-secondary">
-                  
+                <div className="row">
+                  <div className="col-md-6">
+                    <div
+                      className="mb-3 mt-4"
+                      onDrop={handleDrop}
+                      onDragOver={handleDragOver}
+                    >
+                      <label className="mb-1" htmlFor="HTMLContent">
+                        Contenido HTML:
+                      </label>
+                      <textarea
+                        placeholder="Arrastra y suelta etiquetas aquí para posicionarlas o editarlas"
+                        value={contenidoHtml}
+                        onChange={handleTextAreaChange}
+                        rows={16}
+                        cols={100}
+                        style={{ resize: "none", spellCheck: "false" }}
+                        className="form-control"
+                        name="HTMLContent"
+                        required
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="mb-3 mt-4">
+                      <label className="mb-1">Resultado:</label>
+                      <iframe
+                        title="Vista previa del sitio web"
+                        srcDoc={previewHtml}
+                        style={{
+                          width: "100%",
+                          height: "400px",
+                          border: "1px solid #ccc",
+                          borderRadius: "5px",
+                        }}
+                      ></iframe>
+                    </div>
+                  </div>
                 </div>
-              </div>
-
+              </form>
             </div>
-
           </div>
         </div>
       </div>
     </div>
   );
-}
-
+};
 
 export default Paginascreate;
